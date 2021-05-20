8b13817f38334db3873a017ef660925d26d48820
zxt235813@163.com <zxt235813@163.com>
2021-05-20 11:15:24 +0800

    transformer

--- net/model.py
+++ net/model.py
@@ -16,30 +16,82 @@ class FesNet(nn.Module):
     """
     
     """
-    def __init__(self, element_dim, step_size, embedding_dim, hidden_dim):
+    def __init__(self, element_dim, step_size=100, hidden_dim=153, nhead=9, num_layers=1):
         super(FesNet, self).__init__()
-        self.embedding_dim = embedding_dim
-        self.hidden_dim = hidden_dim
+        #self.embedding_dim = embedding_dim
+        #self.hidden_dim = hidden_dim
 
-        self.lstm = nn.LSTM(element_dim, hidden_dim)
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=element_dim, nhead=nhead)
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
+
+        self.decoder_layer = nn.TransformerDecoderLayer(d_model=element_dim, nhead=nhead)
+        self.transformer_decoder = nn.TransformerDecoder(self.decoder_layer, num_layers=num_layers)
 
     
-    def forward(self, sentences, length_list, targets):
-        out = self.get_lstm_feature(sentences, length_list)
+    def forward(self, structures, sentences):
+        memory = self.transformer_encoder(structures)
 
-        return self.crf(torch.transpose(out, 0, 1), torch.transpose(targets, 0, 1), torch.transpose(self.get_mask(length_list), 0, 1))
+        out = self.transformer_decoder(sentences, memory)
 
-    def predict(self, sentences, length_list):
-        out = self.get_lstm_feature(sentences, length_list)
-
-        return self.crf.decode(torch.transpose(out, 0, 1), torch.transpose(self.get_mask(length_list), 0, 1))
+        return out
 
 
+def pretty_time(time, degree=1):
+    return str(int(time // 60)) + "m" + str(round(time % 60, degree)) if time > 60 else round(time, degree)
+
+
+def test_train(net, src, tgt, epoch=50):
+    
+    criterion =  nn.MSELoss()
+
+    optimizer = optim.Adam(net.parameters(), lr=0.005)
+
+    start_time = time.time()
+
+    loss_list = []
+
+    print(src.shape, tgt.shape)
+
+    for i in range(epoch):
+        total_loss = 0.
+
+        probs = net(src, tgt)
+
+        net.zero_grad()
+  
+        loss = criterion(probs.reshape(-1), tgt.reshape(-1))
+
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(net.parameters(), 0.5)
+        optimizer.step()
+
+        total_loss += loss.item()
+        if (i+1) % 5 == 0:
+            time_past = time.time() - start_time
+            cur_loss = total_loss
+            loss_list.append(cur_loss)
+            total_loss = 0
+            print("epoch: ", i, " time: ", pretty_time(time_past), "loss: ", cur_loss)
+
+    plt.plot(loss_list)
+
+    plt.show()
 
 
 def run():
-    return
+    fesnet = FesNet(element_dim=153)
 
+    structures = torch.rand(6, 1, 153)
+    
+    sentences = torch.rand(101, 1,153)
+
+    out = fesnet(structures, sentences)
+
+    print(out.shape)
+
+    test_train(net=fesnet, src=structures, tgt=sentences)
+
+    return
 
 
 if __name__ == "__main__":
